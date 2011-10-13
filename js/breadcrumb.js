397d43726c18b02ed8d795d7456b821f43635f81
Chris Scheller <schelcj@pobox.com>
2011-10-13 11:31:35 -0400

    now have a working auto generated breadcrumb

---
+++ js/breadcrumb.js
@@ -0,0 +1,45 @@
+var breadcrumb_map = {
+  sph : {
+    path: '/',
+    title: 'UM SPH Home',
+  },
+
+  biostat : {
+    path: '/biostat/',
+    title: 'Department of Biostatistics',
+  },
+
+  computing : {
+    path: '/biostat/computing/',
+    title: 'Biostatistics Computing Resources'
+  },
+
+  cluster : {
+    path: '/biostat/compute/cluster/',
+    title: 'Biostatistics Cluster Documentation',
+  },
+
+  compute_servers : {
+    path: '/biostat/computing/compute_servers',
+    title: 'Compute Servers',
+  }
+};
+
+$(function() {
+  var crumbs = $('meta[name=breadcrumb]').attr('content').split('|');
+
+  if (crumbs.length >= 0) {
+    $(crumbs).each(function(index, element) {
+        $('<a />', {
+          href:  breadcrumb_map[element].path, 
+          title: breadcrumb_map[element].title, 
+          text:  breadcrumb_map[element].title,
+          class: 'breadcrumbs'
+        }).appendTo('td.breadcrumb'); 
+
+        if (index != (crumbs.length - 1)) {
+          $('td.breadcrumb').append(' &gt; ');
+        }
+    });
+  }
+});
